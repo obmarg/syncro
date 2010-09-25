f585e9860591f9ee7a65c8c759c23103809fffb9
Grambo <grambo@grambo.me.uk>
2010-09-25 20:29:23 +0100

    Server: Did more work on CXMLReceiveHandler

--- Syncro Server/Syncro Server/XMLHandlers.cpp
+++ Syncro Server/Syncro Server/XMLHandlers.cpp
@@ -14,3 +14,19 @@ bool CXMLAcceptHandler::HandleAccept(CTCPConnection::TPointer inpNewConnection) 
 	inpNewConnection->StartRecv( 0 );
 	return true;
 }
+
+bool CXMLReceiveHandler::CanHandleReceive(const TCharBuffer& inoBuffer) {
+	if(inoBuffer.nSize > 3) {
+		if( (inoBuffer.aBuffer[0] == 'X') && (inoBuffer.aBuffer[1] == 'M') && (inoBuffer.aBuffer[2] == 'L') ) {
+			if( inoBuffer.nSize > (3 + sizeof(long)) ) {
+				long nExpectedSize = *((long*)&inoBuffer.aBuffer[3]);
+			}
+		}
+	}
+	return false;
+}
+
+bool CXMLReceiveHandler::HandleReceive(const TCharBuffer& inoBuffer) {
+	//TODO: Do something
+	return true;
+}
