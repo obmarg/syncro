b97c8125a1edeca9870cf9e46139768a063240d8
Grambo <grambo@grambo.me.uk>
2010-10-03 19:39:56 +0100

    Client: Added protocol buffer generated files

--- Messages/build.bat
+++ Messages/build.bat
@@ -1,2 +1,4 @@
 protoc -I=. --cpp_out "..\Syncro Server\Syncro Server\protocol_buffers" binarydata.proto
+protoc -I=. --java_out "..\Syncro\src" binarydata.proto
 protoc -I=. --cpp_out "..\Syncro Server\Syncro Server\protocol_buffers" header.proto
+protoc -I=. --java_out "..\Syncro\src" header.proto
