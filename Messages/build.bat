21e8088137d624bfd155e783c76025799ab5962c
Grambo <grambo@grambo.me.uk>
2010-11-20 17:06:41 +0000

    Protocol Buffers: Added folders message & updated build.bat

--- Messages/build.bat
+++ Messages/build.bat
@@ -1,8 +1,12 @@
-protoc -I=. --cpp_out "..\Syncro Server\Syncro Server\protocol_buffers" binarydata.proto
+protoc -I=. --cpp_out "gen/" binarydata.proto
 protoc -I=. --java_out "..\Syncro\src" binarydata.proto
-protoc -I=. --cpp_out "..\Syncro Server\Syncro Server\protocol_buffers" header.proto
+protoc -I=. --cpp_out "gen/" header.proto
 protoc -I=. --java_out "..\Syncro\src" header.proto
-protoc -I=. --cpp_out "..\Syncro Server\Syncro Server\protocol_buffers" handshake.proto
+protoc -I=. --cpp_out "gen/" handshake.proto
 protoc -I=. --java_out "..\Syncro\src" handshake.proto
-protoc -I=. --cpp_out "..\Syncro Server\Syncro Server\protocol_buffers" admin.proto
+protoc -I=. --cpp_out "gen/" admin.proto
 protoc -I=. --java_out "..\Syncro\src" admin.proto
+protoc -I=. --cpp_out "gen/" folders.proto
+protoc -I=. --java_out "..\Syncro\src" folders.proto
+move "gen\*.pb.cc" "..\Syncro Server\libsyncro\src\protocol_buffers\"
+move "gen\*.pb.h" "..\Syncro Server\libsyncro\include\libsyncro\protocol_buffers\"
