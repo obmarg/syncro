2ce58bb996fc4a3e35bf91314a0f4c590cde1a09
Grambo <grambo@grambo.me.uk>
2010-12-12 17:42:43 +0000

    SyncroAdmin: Added basic CLO support & NetworkException handling

--- Syncro Server/Tools/SyncroAdmin/SyncroAdmin.cpp
+++ Syncro Server/Tools/SyncroAdmin/SyncroAdmin.cpp
@@ -2,27 +2,57 @@
 //
 
 #include <libsyncro/connection.h>
+#include <kode/net.h>
+#include <boost/program_options.hpp>
+#include <iostream>
+
+namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
 	using namespace syncro::client;
 
-	Connection conn(
-		ConnectionDetails()
-			.SetUsername( "grambo" )
-			.SetPassword( "password" )
-			.SetHostname( "localhost" )
-		);
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "produce help message")
+		("addfolder,a", po::value< std::string >(), "add folder");
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);    
 
-	syncro::FolderList list;
+	if( vm.count("help") )
+	{
+		std::cout << desc << "\n";
+		return 1;
+	}
 
-	conn.GetFolderList( list );
+	try 
+	{
+		Connection conn(
+			ConnectionDetails()
+				.SetUsername( "grambo" )
+				.SetPassword( "password" )
+				.SetHostname( "localhost" )
+			);
 
-	conn.SendAdminCommand("AddFolder","C:\\temp\\");
+		if( vm.count("addfolder") )
+		{
+			conn.SendAdminCommand( "AddFolder", vm["addfolder"].as<std::string>() );
+		}
 
-	int i = 0;
-	i += 1;
+	/*	syncro::FolderList list;
 
+		conn.GetFolderList( list );
+
+		conn.SendAdminCommand("AddFolder","C:\\temp\\");
+
+		int i = 0;
+		i += 1;*/
+	}
+	catch( const kode::net::NetworkException& ex )
+	{
+		std::cout << "Network Exception: " << ex.what() << "\n";
+	}
 	return 0;
 }
 
