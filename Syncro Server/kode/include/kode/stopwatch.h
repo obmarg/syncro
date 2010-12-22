cfc7c141061116eb8f7054d3ddd18a913f190dae
Grambo <grambo@grambo.me.uk>
2010-12-22 00:06:01 +0000

    kode: Added stopwatch class

---
+++ Syncro Server/kode/include/kode/stopwatch.h
@@ -0,0 +1,32 @@
+#ifndef KODE_STOPWATCH_H_
+#define KODE_STOPWATCH_H_
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+namespace kode {
+namespace time { 
+
+class Stopwatch
+{
+public:
+	Stopwatch()
+		:m_running( false )
+	{
+		Start();
+	}
+
+	void Start();
+	void Reset();
+	void Stop();
+
+	int64_t GetMS();
+private:
+	boost::posix_time::ptime m_startTime;
+	boost::posix_time::ptime m_finishTime;
+	bool m_running;
+};
+
+}	// namespace time
+}	// namespace kode 
+
+#endif
---
+++ Syncro Server/kode/src/stopwatch.cpp
@@ -0,0 +1,47 @@
+#include "stopwatch.h"
+
+namespace kode {
+namespace time { 
+
+void Stopwatch::Start()
+{
+	if( !m_running )
+		Reset();
+}
+
+void Stopwatch::Reset()
+{
+	m_startTime = 
+		boost::posix_time::microsec_clock::universal_time();
+	m_running = true;
+}
+
+void Stopwatch::Stop()
+{
+	if( m_running )
+	{
+		m_finishTime = 
+			boost::posix_time::microsec_clock::universal_time();
+		m_running = false;
+	}
+}
+
+int64_t Stopwatch::GetMS()
+{
+	boost::posix_time::time_duration duration;
+	if( m_running )
+	{
+		duration = 
+			boost::posix_time::microsec_clock::universal_time()
+			- m_startTime
+			;
+	}
+	else
+	{
+		duration = m_finishTime - m_startTime;
+	}
+	return duration.total_milliseconds();
+}
+
+}	// namespace time
+}	// namespace kode 
