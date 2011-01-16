a0ff7e55bc2adf9426d8f61c0b506671fd8fbf78
Graeme Coupar <grambo@grambo.me.uk>
2011-01-16 18:28:32 +0000

    Server: Switched to HMAC password authentication

--- Syncro Server/Syncro Server/src/AuthManager.cpp
+++ Syncro Server/Syncro Server/src/AuthManager.cpp
@@ -1,12 +1,48 @@
 #include "AuthManager.h"
 #include "SyncroDB.h"
+#include <kode/base64.h>
+#include <cryptopp/hmac.h>
+#include <iostream>
 
 namespace syncro {
 
 CAuthManager::CAuthManager() {
 	m_pDB = CSyncroDB::OpenDB();
 	m_pCheckLoginStatement = m_pDB->prepare("SELECT ID,Password FROM Users WHERE UserName=@Username");
-	m_salt = kode::utils::GenerateUUID();
+	std::string uuid = kode::utils::GenerateUUID();
+	
+	CryptoPP::SHA().CalculateDigest( 
+		m_salt, 
+		reinterpret_cast< const unsigned char* >( uuid.c_str() ), 
+		uuid.length() 
+		);
+#if 0
+	CryptoPP::Base64Encoder encoder;
+	encoder.PutMessageEnd( 
+		m_salt,
+	       	sizeof( m_salt ) 
+		);
+	if( !encoder.AnyRetrievable() )
+		throw std::runtime_error( "Base64 error in CAuthManager::CAuthManager()" );
+
+	unsigned int destSize = encoder.MaxRetrievable();
+	std::vector< char > saltVector( destSize );
+
+	unsigned int sizeRead = encoder.Get( 
+		reinterpret_cast< unsigned char* >( &saltVector[0] ),
+		destSize
+		);
+	if( sizeRead != destSize )
+	{
+		std::cout << "Warning: Salt vector read legnth is less than expected in " <<
+			"CAuthManager::CAuthManager()\n";
+		saltVector.resize( sizeRead );
+	}
+
+	m_saltString = std::string( saltVector.begin(), saltVector.end() );
+#endif
+	m_saltString = kode::base64::Encode( m_salt, sizeof( m_salt ) );
+	std::cout << "Salt: " << m_saltString << "\n";
 }
 
 CAuthManager::~CAuthManager() {
@@ -19,18 +55,24 @@ bool CAuthManager::NeedsAuth() {
 
 const CAuthToken CAuthManager::Authenticate(const std::string& username, const std::string& password, const std::string& ip) {
 	kode::db::AutoReset autoReset( m_pCheckLoginStatement );
+	
 	m_pCheckLoginStatement->Bind( "@Username", username );
-	//m_pCheckLoginStatement->Bind( "@Password", password );
-	/*m_pCheckLoginStatement->Bind( "@IP", ip );*/
 	if( m_pCheckLoginStatement->GetNextRow() ) {
 		int nID = m_pCheckLoginStatement->GetColumn<int>( 0 );
-		std::string passwordHash = m_pCheckLoginStatement->GetColumn<std::string>( 1 );
-		
+		std::string passwordHash = m_pCheckLoginStatement->GetColumn<std::string>( 1 );	
+
 		//
 		// Now we need to confirm the passwords match
 		//
-
-		//TODO: need to implement password hashing shit...
+		unsigned char hashBuffer[ CryptoPP::SHA::DIGESTSIZE ];
+		CryptoPP::HMAC< CryptoPP::SHA >( m_salt, sizeof( m_salt ) )
+			.CalculateDigest( 
+				hashBuffer, 
+				reinterpret_cast< const unsigned char* >( passwordHash.c_str() ), 
+				passwordHash.length()
+				);
+		
+		
 		if( passwordHash != password )
 			throw authentication_exception("Invalid username or password");
 		return CAuthToken( username, nID, CAuthToken::AccessLevel_Admin );
--- Syncro Server/Syncro Server/src/include/AuthManager.h
+++ Syncro Server/Syncro Server/src/include/AuthManager.h
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include <kode/Database.h>
+#include <cryptopp/sha.h>
 #include <boost/shared_ptr.hpp>
 #include <stdint.h>
 
@@ -15,14 +16,15 @@ public:
 
 	bool NeedsAuth();
 	const CAuthToken Authenticate(const std::string& username, const std::string& password, const std::string& ip);
-	const std::string Salt() const { return m_salt; };
+	const std::string Salt() const { return m_saltString; };
 
 	const CAuthToken DefaultAuth();
 
 private:
 	kode::db::DatabasePtr m_pDB;
 	kode::db::StatementPtr m_pCheckLoginStatement;
-	std::string m_salt;
+	std::string m_saltString;
+	unsigned char m_salt[ CryptoPP::SHA::DIGESTSIZE ];
 };
 
 }
