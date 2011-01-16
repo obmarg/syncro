fdc6c24b1f84c67cd286c48b5b32020647147ea7
Graeme Coupar <grambo@grambo.me.uk>
2011-01-16 21:09:04 +0000

    Added some linux build scripts

---
+++ Messages/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+protoc -I=. --cpp_out "gen/" binarydata.proto
+protoc -I=. --java_out "../Syncro/src" binarydata.proto
+protoc -I=. --cpp_out "gen/" header.proto
+protoc -I=. --java_out "../Syncro/src" header.proto
+protoc -I=. --cpp_out "gen/" handshake.proto
+protoc -I=. --java_out "../Syncro/src" handshake.proto
+protoc -I=. --cpp_out "gen/" admin.proto
+protoc -I=. --java_out "../Syncro/src" admin.proto
+protoc -I=. --cpp_out "gen/" folders.proto
+protoc -I=. --java_out "../Syncro/src" folders.proto
+mv gen/*.pb.cc "../Syncro Server/libsyncro/src/protocol_buffers/"
+mv gen/*.pb.h "../Syncro Server/libsyncro/include/libsyncro/protocol_buffers/"
---
+++ Syncro Server/build.sh
@@ -0,0 +1,8 @@
+cd ../Dependencies/
+scons
+cd ../Syncro\ Server/kode/
+scons
+cd ../libsyncro/
+scons
+cd ../Syncro\ Server/
+scons
